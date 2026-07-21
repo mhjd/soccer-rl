caa3a8b72de41dc5a504c5292d2fdc60920efd14
habib <pbllemb@gmail.com>
2026-07-21 11:38:22 +0200

    adding metrics to evaluate script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,16 +1,31 @@
 from stable_baselines3 import PPO
 from src.env import SoccerEnv
 
+NUM_EPISODES = 100
 
 env = SoccerEnv(render_mode="human")
 
 model = PPO.load("models/ppo_soccer")
 
-for i in range(10):
+total_reward = 0
+success = 0
+
+steps = 0
+for i in range(NUM_EPISODES):
     obs, info = env.reset()
     terminated = False
     truncated = False
     while not (terminated or truncated):
         action, _states = model.predict(obs, deterministic=True)
         obs, reward, terminated, truncated, info = env.step(action)
+        total_reward += reward
+        steps += 1
+        if terminated:
+            success += 1
+        
 env.close()
+
+print(f"goals scored : {success}")
+print(f"Success rate: {success/NUM_EPISODES*100}%")
+print(f"mean episode length: {steps/NUM_EPISODES}")
+print(f"mean reward by episode : {total_reward/NUM_EPISODES}")
